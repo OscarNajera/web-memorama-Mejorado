96dedf3804c0be24d107e046a443bcf21dd5f080
Oscar Najera <najerao87@gmail.com>
2021-08-19 14:04:05 -0500

    --script : se comenta el script y se crea la funcion aleatorio para mesclar el contenido de un array --html se eliminan  las etiquetas inecesarias de muestra de carta-box

--- script/script.js
+++ script/script.js
@@ -10,27 +10,65 @@ const fondo =  document.getElementById('fondo');
 
 
 cargarImg.addEventListener('change', (e) => {
-	const  archivos=  e.target.files;
-    console.log("s", archivos);
-    const fragment =  document.createDocumentFragment();
-    for(let archivo of archivos   ){
+	const  archivos=  e.target.files;						//variable que contiene lso ficheros
+    const fragment =  document.createDocumentFragment();	//variable para realizar fragmento de html y posterior mente incorporarlo (optimizacion)
+    const replica = [];										//variable que duplicara el valor de los archivos
+    replica.push(...archivos,...archivos);					//se pasan 2 veces el valor de los archivos
+    const replicaaleatoria = aleatoriox(replica);			//esta avriable revuelve el contenido de un array		
+
+
+    for(let archivo of replicaaleatoria   ){
 
     	let xfiltro= filtro(archivo.name);               //filtro exclusivo para imagenes
     	if (xfiltro) {
+
+
 	    	let leerArchivo=new FileReader();            //instanciamos la funcion de lectura de archivo
-	    	let img = document.createElement("img");      //creamos el elemento imagen
-	    	leerArchivo.readAsDataURL(archivo);         //leeremos cada archivo del bucle
-			leerArchivo.addEventListener("load", (e) =>{ //evenmto de escucha hasta que se cargaron Todas las imagenes
-				 img.setAttribute('src', e.target.result);
-				 console.log('----------',archivo.name)
-			} )
-			fragment.appendChild(img)					 //se añaden todas las imagenes al fragmento
+
+
+	    									//crearemos las etiquetas que contendran el efeto de carata
+	    	let cartaBox= document.createElement("div"); 
+	    	let carta= document.createElement("div"); 
+	    	let cara= document.createElement("div"); 
+	    	let detras= document.createElement("div"); 
+
+			let img = document.createElement("img");     
+			let imgf =document.createElement("img");
+
+											//le proporcionamos a las etiquetas sus respectivas clases 
+			cartaBox.setAttribute('class', 'carta-box');
+			carta.setAttribute('class', 'carta');
+			cara.setAttribute('class', 'cara');
+			detras.setAttribute('class', 'cara detras');
+
+
+	    	leerArchivo.readAsDataURL(archivo);         //leeremos el contenido del archivo 
+
+					//evenmto de escucha hasta que se cargaron Todas las imagenes en el cual se asignara el valor de la imagen 
+			leerArchivo.addEventListener("load", (e) =>{ 
+			img.setAttribute('src', e.target.result);
+			img.setAttribute('alt', archivo.name);
+
+			console.log('-nombre del archivo',archivo.name)
+			})
+
+											//añadimos las respectivas etiquetas a sus complementos 
+			imgf.setAttribute('src', 'https://asturias24.es/wp-content/plugins/pepeTarot/img/carta.png')
+			cara.appendChild(imgf);
+			detras.appendChild(img);
+			carta.appendChild(cara);
+			carta.appendChild(detras);
+			cartaBox.appendChild(carta);
+
+
+			fragment.appendChild(cartaBox)					 //se añaden todas las imagenes al fragmento
 	    
     	}
     	else{ console.log("archivo no valido");  }
 
 
     }
+
     verimg.appendChild(fragment)					 //se añade el fragmento creado con todas las imagenes (optimizacion)
 
 })
@@ -38,7 +76,7 @@ cargarImg.addEventListener('change', (e) => {
 
 rango.addEventListener("input" , (e)=>{
 	console.log(' e = ' ,e.target.value);
-	let ancho= e.target.value;
+	let ancho= e.target.value; 
 	verimg.style.width = `${ancho}%`;
 })
 
@@ -49,6 +87,10 @@ color.addEventListener('input', (e) =>{
 	fondo.style.backgroundColor = xce;
 })
 
+
+
+///////////////////////////Funciones
+
 const  filtro = (x)=>{
 	
 	let formatos= /(\.jpg|\.png|\.bmp|\.gif|\.jpeg|\.svg|\.tiff|\.ico|\.eps|\.svg)/gmi;
@@ -57,6 +99,24 @@ const  filtro = (x)=>{
 
 	return valida
 }
+
+
+
+////////////////////////////// Random para arrays
+
+const aleatoriox = (e)=>{
+	let  mesclado=[];                                       //variable que almacenara el valor aleatorio
+	let c = e.length;
+	let mescla=e;
+	 
+ 	while(mesclado.length <  c){
+ 		let r = Math.floor(Math.random() * mescla.length);  //valor random del array
+ 		mesclado.push(mescla[r]); 							//el valor random siempre se coloca en la primera posicion
+ 		mescla.splice(r,1);									//el valor extraido se elimina del array lo que provoca que disminulla su tamaño (optimizacion)
+ 	}
+	return mesclado	
+
+} 
 
 
  
